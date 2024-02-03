c34476a4a6f75dfc48afafe78cb060c119a4e01b
andxbes <andxbes@gmail.com>
2024-02-03 17:20:01 +0200

    153

--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -1,17 +1,34 @@
+import { useRef } from "react";
 import Input from "./Input";
 
-export default function NewProject() {
+export default function NewProject({ oneAdd }) {
+    const title = useRef();
+    const description = useRef();
+    const dueDate = useRef();
+
+    function handleSave() {
+        const entredTitle = title.current.value;
+        const entredDescription = description.current.value;
+        const entredDueDate = dueDate.current.value;
+
+        oneAdd({
+            title: entredTitle,
+            description: entredDescription,
+            dueDate: entredDueDate
+        });
+    }
+
     return (
         <div className="w-[35rem]  mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-slate-50 hover:bg-stone-950">Save</button></li>
+                <li><button className="px-6 py-2 rounded-md bg-stone-800 text-slate-50 hover:bg-stone-950" onClick={handleSave}>Save</button></li>
             </menu>
             <div>
-                <Input label="Title" />
-                <Input label="Description" textArea />
-                <Input label="Due Date" />
+                <Input type="text" label="Title" ref={title} />
+                <Input label="Description" textArea ref={description} />
+                <Input type="date" label="Due Date" ref={dueDate} />
             </div>
-        </div >
+        </div>
     );
 }
